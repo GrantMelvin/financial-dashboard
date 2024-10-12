8b1b7b49182eb9863a20d80e225ebad40ccede4d
GrantMelvin <Grantmelvin4@gmail.com>
2024-10-12 18:57:16 -0400

    documentation

--- robinhood.py
+++ robinhood.py
@@ -14,11 +14,11 @@ def email_alert(positive_change):
   msg['from'] = os.getenv("EMAIL")
 
   if(positive_change):
-    msg.set_content('WE\'RE UP BIG')
     msg['subject'] = 'GOOD'
+    msg.set_content('STONKS ARE UP')
   else:
-    msg.set_content('WE\'RE DOWN BAD')
     msg['subject'] = 'BAD'
+    msg.set_content('STONKS ARE DOWN')
 
   server = smtplib.SMTP("smtp.gmail.com", 587)
   server.starttls()
@@ -48,7 +48,7 @@ def main():
   equity = 0
 
   while(1):
-    # Get your portfolio information
+    # Current and old portfolio information 
     old_equity = equity
     portfolio = r.profiles.load_portfolio_profile()
     equity = float(portfolio['equity'])
@@ -56,7 +56,7 @@ def main():
 
     if((old_equity < equity) and (old_equity != 0)):
       email_alert(positive_change=True)
-    
+
     if((old_equity > equity) and (old_equity != 0)):
       email_alert(positive_change=False)
 
@@ -64,7 +64,6 @@ def main():
     # quantity            how much of it I own
     # average_buy_price   how much I bought it for
     positions = r.get_open_stock_positions()
-    # print(json.dumps(positions, indent=4))
 
     # Gets symbols
     tickers = [r.get_symbol_by_url(item["instrument"]) for item in positions]
@@ -74,26 +73,28 @@ def main():
     # ask_price          how much it is selling for rn
     # prev_close(?)      how much it closed for yesterday
     quotes = r.get_quotes(tickers)
-    # print(json.dumps(quotes, indent=4))
 
+    # Creates a new table every iteration
     table = PrettyTable()
     table.field_names = ["Stock", "Quantity", "Buy Price", "Total Buy Value", "Total Current Value", "Profit"]
+
+    # Gathers individual stock info and populates table
     for stock in positions:
       name                = stock['symbol']
       last_price          = getLastPrice(quotes, stock)
-
       quantity            = round(float(stock['quantity']), 3)
       buy_price           = round(float(stock['average_buy_price']), 3)
       total_buy_value     = round(float(stock['quantity']) * float(stock['average_buy_price']), 3)
       total_current_value = round(float(stock['quantity']) * last_price, 3)
       profit_per_stock    = round(total_current_value - total_buy_value, 3)
+      profit              = profit - total_buy_value
 
       table.add_row([name, quantity, buy_price, total_buy_value, total_current_value, profit_per_stock])
-      profit -= total_buy_value
 
+    # Prints table every iteration
     show(table, equity, profit)
 
-    # Print every 10min
+    # Update every 10 minutes
     time.sleep(60 * 10)
 
 if __name__ == "__main__":
