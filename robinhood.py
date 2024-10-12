19c9fd4e6f9f86be6815119f36fe39ab6a475f7e
GrantMelvin <Grantmelvin4@gmail.com>
2024-10-12 19:15:31 -0400

    runs every 6 hours

--- robinhood.py
+++ robinhood.py
@@ -8,6 +8,13 @@ import os
 import json
 import time
 
+def processMessage(msg):
+  server = smtplib.SMTP("smtp.gmail.com", 587)
+  server.starttls()
+  server.login(os.getenv("EMAIL"), os.getenv("GMAILPASS"))
+  server.send_message(msg)
+  server.quit()
+
 def email_alert(positive_change):
   msg = EmailMessage()
   msg['to']   = os.getenv("EMAIL")
@@ -20,12 +27,8 @@ def email_alert(positive_change):
     msg['subject'] = 'BAD'
     msg.set_content('STONKS ARE DOWN')
 
-  server = smtplib.SMTP("smtp.gmail.com", 587)
-  server.starttls()
-  server.login(os.getenv("EMAIL"), os.getenv("GMAILPASS"))
-  server.send_message(msg)
-  server.quit()
-
+  # Sends the email from the smtp server
+  processMessage(msg)
 
 def getLastPrice(quotes, stock):
   for item in quotes:
@@ -36,6 +39,23 @@ def show(table, equity, profit):
   print(table)
   print(f"\nPortfolio Value: ${round(equity, 2)}")
   print(f"Profit Value:    ${round(profit, 2)}")
+
+def handleEquity(old, new):
+  email_alert(positive_change=True)
+  if((old < new) and (old != 0)):
+    email_alert(positive_change=True)
+
+  if((old > new) and (old != 0)):
+    email_alert(positive_change=False)
+
+def validTime():
+  if((time.localtime().tm_hour == 6 and time.localtime().tm_min == 0) or
+       (time.localtime().tm_hour == 12 and time.localtime().tm_min == 0) or
+       (time.localtime().tm_hour == 18 and time.localtime().tm_min == 0) or 
+       (time.localtime().tm_hour == 24 and time.localtime().tm_min == 0)):
+    return True
+  
+  return False
 
 def main():
   load_dotenv()
@@ -48,54 +68,56 @@ def main():
   equity = 0
 
   while(1):
-    # Current and old portfolio information 
-    old_equity = equity
-    portfolio = r.profiles.load_portfolio_profile()
-    equity = float(portfolio['equity'])
-    profit = equity
+    # If we want to check every 6 hours
+    if(validTime):
+      # Current and old portfolio information 
+      old_equity = equity
+      portfolio = r.profiles.load_portfolio_profile()
+      equity = float(portfolio['equity'])
 
-    if((old_equity < equity) and (old_equity != 0)):
-      email_alert(positive_change=True)
+      # Checks new and old equity -> email if changed
+      handleEquity(old_equity, equity)
 
-    if((old_equity > equity) and (old_equity != 0)):
-      email_alert(positive_change=False)
+      # Need:
+      # quantity            how much of it I own
+      # average_buy_price   how much I bought it for
+      positions = r.get_open_stock_positions()
 
-    # Need:
-    # quantity            how much of it I own
-    # average_buy_price   how much I bought it for
-    positions = r.get_open_stock_positions()
+      # Gets symbols
+      tickers = [r.get_symbol_by_url(item["instrument"]) for item in positions]
 
-    # Gets symbols
-    tickers = [r.get_symbol_by_url(item["instrument"]) for item in positions]
+      # Need:
+      # symbol             what stock it is
+      # ask_price          how much it is selling for rn
+      # prev_close(?)      how much it closed for yesterday
+      quotes = r.get_quotes(tickers)
 
-    # Need:
-    # symbol             what stock it is
-    # ask_price          how much it is selling for rn
-    # prev_close(?)      how much it closed for yesterday
-    quotes = r.get_quotes(tickers)
+      # Creates a new table every iteration
+      table = PrettyTable()
+      table.field_names = ["Stock", "Quantity", "Buy Price", "Total Buy Value", "Total Current Value", "Profit"]
+      profit = equity
 
-    # Creates a new table every iteration
-    table = PrettyTable()
-    table.field_names = ["Stock", "Quantity", "Buy Price", "Total Buy Value", "Total Current Value", "Profit"]
+      # Gathers individual stock info and populates table
+      for stock in positions:
+        name                = stock['symbol']
+        last_price          = getLastPrice(quotes, stock)
+        quantity            = round(float(stock['quantity']), 3)
+        buy_price           = round(float(stock['average_buy_price']), 3)
+        total_buy_value     = round(float(stock['quantity']) * float(stock['average_buy_price']), 3)
+        total_current_value = round(float(stock['quantity']) * last_price, 3)
+        profit_per_stock    = round(total_current_value - total_buy_value, 3)
+        profit              = profit - total_buy_value
 
-    # Gathers individual stock info and populates table
-    for stock in positions:
-      name                = stock['symbol']
-      last_price          = getLastPrice(quotes, stock)
-      quantity            = round(float(stock['quantity']), 3)
-      buy_price           = round(float(stock['average_buy_price']), 3)
-      total_buy_value     = round(float(stock['quantity']) * float(stock['average_buy_price']), 3)
-      total_current_value = round(float(stock['quantity']) * last_price, 3)
-      profit_per_stock    = round(total_current_value - total_buy_value, 3)
-      profit              = profit - total_buy_value
+        table.add_row([name, quantity, buy_price, total_buy_value, total_current_value, profit_per_stock])
 
-      table.add_row([name, quantity, buy_price, total_buy_value, total_current_value, profit_per_stock])
+      # Prints table every iteration
+      show(table, equity, profit)
 
-    # Prints table every iteration
-    show(table, equity, profit)
+      # Skip a minute so that we don't run it twice on the same interval
+      time.sleep(60)
 
-    # Update every 10 minutes
-    time.sleep(60 * 10)
+    # Skips 6 hours - should work but might now
+    time.sleep(60 * 60 * 6)
 
 if __name__ == "__main__":
   main()
